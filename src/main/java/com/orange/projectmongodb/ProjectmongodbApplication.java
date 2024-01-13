8ac5ba0bb6964680fc3084016ed6f6f36d8e524c
danielorangeviana <danieloviana@gmail.com>
2024-01-13 12:27:23 -0300

    first commit and create project

---
+++ src/main/java/com/orange/projectmongodb/ProjectmongodbApplication.java
@@ -0,0 +1,13 @@
+package com.orange.projectmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectmongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectmongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/orange/projectmongodb/ProjectmongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orange.projectmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectmongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
