fcfa348d849d4b8278045151035a75907b65c9f4
danielorangeviana <danieloviana@gmail.com>
2024-01-16 17:24:42 -0300

    Inserção de usuário com POST

--- src/main/java/com/orange/projectmongodb/services/UserService.java
+++ src/main/java/com/orange/projectmongodb/services/UserService.java
@@ -7,21 +7,31 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.orange.projectmongodb.domain.User;
+import com.orange.projectmongodb.dto.UserDTO;
 import com.orange.projectmongodb.repository.UserRepository;
 import com.orange.projectmongodb.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repository;
-	
+
 	public List<User> findAll() {
 		return repository.findAll();
 	}
-	
+
 	public User findById(String id) {
 		Optional<User> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
+
+	public User insert(User obj) {
+		return repository.insert(obj);
+	}
+
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
+
 }
