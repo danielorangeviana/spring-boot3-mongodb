6a826015044e64793b636a579d881f00f9729180
danielorangeviana <danieloviana@gmail.com>
2024-01-23 16:21:17 -0300

    Projeção dos dados do autor com DTO

---
+++ src/main/java/com/orange/projectmongodb/dto/AuthorDTO.java
@@ -0,0 +1,37 @@
+package com.orange.projectmongodb.dto;
+
+import java.io.Serializable;
+
+import com.orange.projectmongodb.domain.User;
+
+public class AuthorDTO implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	private String id;
+	private String name;
+	
+	public AuthorDTO() {
+	}
+
+	public AuthorDTO(User obj) {
+		super();
+		this.setId(obj.getId());
+		this.setName(obj.getName());
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
