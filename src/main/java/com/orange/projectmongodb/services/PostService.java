f760dd6660b78ed8c6d9bc1e6382cd63dcc04d54
danielorangeviana <danieloviana@gmail.com>
2024-01-25 16:23:37 -0300

    Obtendo um post por id

---
+++ src/main/java/com/orange/projectmongodb/services/PostService.java
@@ -0,0 +1,23 @@
+package com.orange.projectmongodb.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.orange.projectmongodb.domain.Post;
+import com.orange.projectmongodb.repository.PostRepository;
+import com.orange.projectmongodb.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repository;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+}
